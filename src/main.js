5ff7606173d5a6ec7bc731e094e37fcf6bd9f9f2
Elvis <elvismendez91@gmail.com>
2024-10-09 19:32:02 +0200

    Proyecto acabado

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
 import "./style.css";
 
-console.log("Hello from main");
+const numeroTurno = document.querySelector(".numero-turno");
+
+const botonAnterior = document.getElementById("boton-anterior");
+const botonSiguiente = document.getElementById("boton-siguiente");
+const botonReset = document.getElementById("reset");
+const botonCambiarTurno = document.getElementById("cambiar-turno");
+
+// Convertir el texto del número a un número entero
+let turnoActual = parseInt(numeroTurno.textContent);
+
+// Función para actualizar el número mostrado
+function actualizarTurno() {
+  numeroTurno.textContent = turnoActual.toString().padStart(2, "0");
+}
+
+// Botones:
+botonAnterior.addEventListener("click", () => {
+  turnoActual = turnoActual - 1;
+  actualizarTurno();
+});
+
+botonSiguiente.addEventListener("click", () => {
+  turnoActual = turnoActual + 1;
+  actualizarTurno();
+});
+
+botonReset.addEventListener("click", () => {
+  turnoActual = 1;
+  document.getElementById("editar-turno").value = "01";
+  actualizarTurno();
+});
+
+botonCambiarTurno.addEventListener("click", () => {
+  turnoActual = parseInt(document.getElementById("editar-turno").value)
+  actualizarTurno();
+});
